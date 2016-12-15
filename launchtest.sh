78c0a01be081f89b1a5837e90017c0e764a2b7d9
nickwinn <technick@gmail.com>
2016-12-14 23:15:53 -0700

    Small fixes

--- launchtest.sh
+++ launchtest.sh
@@ -3,4 +3,5 @@
 #docker run -it --rm --name testplex -p 32400:32400 -p 32469:32469 -p 8181:8181 -v /volume/docker/testplex/config:/config -v /volume:/volume:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged nickflix:base /bin/bash
 
 #docker run -it --rm --name testplex -p 32400:32400 -p 32469:32469 -p 8181:8181 -v /volume/docker/testplex/config:/config -v /volume:/volume:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged nickflix:test
-docker run -d --name testplex -p 32400:32400 -p 32469:32469 -p 8181:8181 -v /volume/docker/testplex/config:/config -v /volume:/volume:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged -e "LC_ALL=en_US.UTF-8" nickflix:test
+#docker run -d --name testplex -p 32400:32400 -p 32469:32469 -p 8181:8181 -v /volume/docker/testplex/config:/config -v /volume:/volume:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged -e "LC_ALL=en_US.UTF-8" nickflix:test
+docker run -d --name testplex -p 32400:32400 -p 32469:32469 -v /volume/docker/testplex/config:/config -v /volume:/volume:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged nickflix:test
--- preplex.sh
+++ preplex.sh
@@ -1,9 +1,13 @@
 # This shell script moves a template preferences file into place before Plex starts for the first time.
+# After the first run, the script will remove the 'disableRemoteSecurity=1'
 
 if [ ! -d /config/Plex\ Media\ Server ]
 then
   mkdir /config/Plex\ Media\ Server
   mv /tmp/Preferences.xml /config/Plex\ Media\ Server/
 else
-  exit 0
+  if grep -q 'disableRemoteSecurity="1"' /config/Plex\ Media\ Server/Preferences.xml
+  then
+    sed -i -e 's/ disableRemoteSecurity="1" / /' /config/Plex\ Media\ Server/Preferences.xml
+  fi
 fi
