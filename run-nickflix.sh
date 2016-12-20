db02a102307e4534a1c1e25aabea43fbee197f76
nickwinn <technick@gmail.com>
2016-12-20 11:30:41 -0700

    easier to manage variables and restarts

--- run-nickflix.sh
+++ run-nickflix.sh
@@ -2,6 +2,19 @@
 
 set -ex
 
+MEDIASRC="/volume"
+MEDIADEST="/config"
+PLEXCONFIGSRC="/volume/docker/nickflix/config"
+PLEXCONFIGDST="/config"
+disableRemoteSecurity="0"
+ManualPortMappingMode="1"
+AcceptedEULA="1"
+PublishServerOnPlexOnlineKey="1"
+PLEXPYCONFIGSRC="/volume/docker/nickflix/plexpy"
+PLEXPYCONFIGDST="/config"
+PLEXPYLOGSRC="$PLEXCONFIGSRC"
+PLEXPYLOGDST="/logs:ro"
+
 #/usr/local/bin/docker-compose pull
 /usr/local/bin/docker-compose kill
 /usr/local/bin/docker-compose rm -f
